f9bcd5a14ddbce0f86b16581b5a55d89e2c5ce25
jaydenlil <Jaydenli1024@gmail.com>
2024-08-26 15:51:55 -0400

    Final Open Challenge Code

--- src/OpenChallengeFinal.py
+++ src/OpenChallengeFinal.py
@@ -1,9 +1,11 @@
 # libraries
 
+
 import sys
-import cv2 # for image processing
-import numpy as np # for image manipulation
+import cv2  # for image processing
+import numpy as np  # for image manipulation
 import time
+
 sys.path.append("/home/pi/TurboPi/")
 from picamera2 import Picamera2
 import HiwonderSDK.Board as Board
@@ -106,7 +108,6 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(key2_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while GPIO.input(key2_pin) == GPIO.HIGH:
     pass
-
 # camera setup
 
 
@@ -116,20 +117,22 @@ picam2.preview_configuration.main.format = "RGB888"
 picam2.preview_configuration.controls.FrameRate = 30
 picam2.preview_configuration.align()
 picam2.configure("preview")
-picam2.set_controls({"Saturation": 1.4, "Brightness": 0.05}) # adjust brightness and saturation of the image
+picam2.set_controls(
+    {"Saturation": 1.4, "Brightness": 0.05}
+)  # adjust brightness and saturation of the image
 picam2.start()
 
 # utility function for pwm angle calculation
 
 
-def pwm(degree): # angle must be adjusted to pwm angle for servo
+def pwm(degree):  # angle must be adjusted to pwm angle for servo
     return round(degree * 11.1 + 500)
 
 
 # utility function for drawing ROI
 
 
-def drawROI(ROI): # draw the rectangular ROI on the image
+def drawROI(ROI):  # draw the rectangular ROI on the image
     image = cv2.line(im, (ROI[0], ROI[1]), (ROI[2], ROI[1]), (0, 255, 255), 4)
     image = cv2.line(im, (ROI[0], ROI[1]), (ROI[0], ROI[3]), (0, 255, 255), 4)
     image = cv2.line(im, (ROI[2], ROI[3]), (ROI[2], ROI[1]), (0, 255, 255), 4)
@@ -210,7 +213,6 @@ while True:
         cv2.CHAIN_APPROX_NONE,
     )
 
-
     # define variables for left and right ROI contour areas
 
     left_area_top = 0
@@ -225,32 +227,26 @@ while True:
         cnt = left_contours_top[i]
         area = cv2.contourArea(cnt)
         left_area_top = max(area, left_area_top)
-        
     for i in range(len(left_contours_bot)):
         cnt = left_contours_bot[i]
         area = cv2.contourArea(cnt)
         left_area_bot = max(area, left_area_bot)
-        
     for i in range(len(right_contours_top)):
         cnt = right_contours_top[i]
         area = cv2.contourArea(cnt)
         right_area_top = max(area, right_area_top)
-        
     for i in range(len(right_contours_bot)):
         cnt = right_contours_bot[i]
         area = cv2.contourArea(cnt)
         right_area_bot = max(area, right_area_bot)
-    
     # combine areas on each side
-    
-    
-    right_area = right_area_bot + right_area_top 
+
+    right_area = right_area_bot + right_area_top
     left_area = left_area_bot + left_area_top
-            
+
     b_mask = cv2.inRange(img_hsv, LOWER_BLUE, UPPER_BLUE)
 
     # find blue contours to detect the lines on the mat
-
 
     contours_blue = cv2.findContours(
         b_mask[ROI4[1] : ROI4[3], ROI4[0] : ROI4[2]],
@@ -265,112 +261,122 @@ while True:
 
     # find orange contours to detect the lines on the mat
 
-
     contours_orange = cv2.findContours(
         o_mask[ROI4[1] : ROI4[3], ROI4[0] : ROI4[2]],
         cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE,
     )[-2]
 
-
     # iterate through the blue and orange contours and store the maximum area
-    
+
     max_blue_area = 0
     max_orange_area = 0
-    
+
     for i in range(len(contours_orange)):
         cnt = contours_orange[i]
         max_orange_area = max(cv2.contourArea(cnt), max_orange_area)
         cnt[:, :, 0] += ROI4[0]  # Add X offset
         cnt[:, :, 1] += ROI4[1]  # Add Y offset
-        cv2.drawContours(im, contours_orange, i, (255, 255, 0), 1) # draw the contours for debug utility
-        
+        cv2.drawContours(
+            im, contours_orange, i, (255, 255, 0), 1
+        )  # draw the contours for debug utility
     for i in range(len(contours_blue)):
         cnt = contours_blue[i]
         max_blue_area = max(cv2.contourArea(cnt), max_blue_area)
         cnt[:, :, 0] += ROI4[0]  # Add X offset
         cnt[:, :, 1] += ROI4[1]  # Add Y offset
-        cv2.drawContours(im, contours_blue, i, (255, 255, 0), 1) # draw the contours for debug utility
-        
-        
-    if track_dir == "none": # find which direction the track is going
-        if max_blue_area > max_orange_area and max_blue_area > LINE_THRESHOLD: # if blue is first seen, the track direction is left (counter clockwise)
-            track_dir = "left"
-        if max_orange_area > max_blue_area and max_orange_area > LINE_THRESHOLD: # if orange is first seen, the track direction is right (clockwise)
-            track_dir = "right"
-            
-    if track_dir == "right": 
+        cv2.drawContours(
+            im, contours_blue, i, (255, 255, 0), 1
+        )  # draw the contours for debug utility
+    if track_dir == "none":  # find which direction the track is going
         if (
-            turn_dir == "right" # if the robot is in a sharp turn but sees the opposite colour, the turn is stopped
+            max_blue_area > max_orange_area and max_blue_area > LINE_THRESHOLD
+        ):  # if blue is first seen, the track direction is left (counter clockwise)
+            track_dir = "left"
+        if (
+            max_orange_area > max_blue_area and max_orange_area > LINE_THRESHOLD
+        ):  # if orange is first seen, the track direction is right (clockwise)
+            track_dir = "right"
+    if track_dir == "right":
+        if (
+            turn_dir
+            == "right"  # if the robot is in a sharp turn but sees the opposite colour, the turn is stopped
             and max_blue_area > LINE_THRESHOLD
             and max_orange_area < LINE_THRESHOLD
         ):
             done_turning = True
             turn_dir = "none"
-        elif max_orange_area > LINE_THRESHOLD: # signals that a turn must occur
+        elif max_orange_area > LINE_THRESHOLD:  # signals that a turn must occur
             # if the turn direction hasn't been changed yet change the turn direction to right
+
             if turn_dir == "none":
                 turn_counter += 1
                 turn_dir = "right"
     elif track_dir == "left":
         if (
-            turn_dir == "left" # if the robot is in a sharp turn but sees the opposite colour, the turn is stopped
+            turn_dir
+            == "left"  # if the robot is in a sharp turn but sees the opposite colour, the turn is stopped
             and max_orange_area > LINE_THRESHOLD
             and max_blue_area < LINE_THRESHOLD
         ):
             done_turning = True
             turn_dir = "none"
-        elif max_blue_area > LINE_THRESHOLD: # signals that a turn must occur
+        elif max_blue_area > LINE_THRESHOLD:  # signals that a turn must occur
             # if the turn direction hasn't been changed yet change the turn direction to left
 
             if turn_dir == "none":
                 turn_counter += 1
                 turn_dir = "left"
-
-    
-
-    if done_turning: # if the robot has just completed a turn, make it continue the turn to assure that it sees the two parallel walls
+    if (
+        done_turning
+    ):  # if the robot has just completed a turn, make it continue the turn to assure that it sees the two parallel walls
         turn_length_counter += 1
 
-        if turn_length_counter > TURN_ITER_LIMIT: # turn for the required amount of time
+        if (
+            turn_length_counter > TURN_ITER_LIMIT
+        ):  # turn for the required amount of time
             turn_length_counter = 0
             done_turning = False
-            
     else:
 
-        if turn_dir == "right": # calculate the servo angle for the current turn
+        if turn_dir == "right":  # calculate the servo angle for the current turn
             servo_angle = MID_SERVO - MAX_TURN_DEGREE
-        elif turn_dir == "left": # calculate the servo angle for the current turn
+        elif turn_dir == "left":  # calculate the servo angle for the current turn
             servo_angle = MID_SERVO + MAX_TURN_DEGREE
-            
         else:
-            turn_length_counter = 0 # reset the turn length counter
-            if left_area < WALL_THRESHOLD: # if the left area is below the threshold, set the servo angle to max for a sharp left turn
+            turn_length_counter = 0  # reset the turn length counter
+            if (
+                left_area < WALL_THRESHOLD
+            ):  # if the left area is below the threshold, set the servo angle to max for a sharp left turn
                 servo_angle = MID_SERVO + MAX_TURN_DEGREE
-            elif right_area < WALL_THRESHOLD: # if the right area is below the threshold, set the servo angle to max for a sharp right turn
+            elif (
+                right_area < WALL_THRESHOLD
+            ):  # if the right area is below the threshold, set the servo angle to max for a sharp right turn
                 servo_angle = MID_SERVO - MAX_TURN_DEGREE
             else:
                 # if in the straight section, calculate the current_difference between the contours in the left and right area
+
                 current_difference = left_area - right_area
-                
+
                 # calculate steering amount using preportional-derivative steering
                 # multiply the current_difference by a constant variable and add the projected error multiplied by another constant
                 # this method gives stable and smooth steering
+
                 servo_angle = MID_SERVO - (
                     current_difference * PG
                     + (current_difference - last_difference) * PD
                 )
-                
-    if turn_counter == MAX_TURNS and not done_turning: #if the total turns has surpassed the amount required, increment the action counter by 1
-        action_counter += 1 # this is to ensure that the robot stops in the middle of the straight section
-        
-        
+    if (
+        turn_counter == MAX_TURNS and not done_turning
+    ):  # if the total turns has surpassed the amount required, increment the action counter by 1
+        action_counter += 1  # this is to ensure that the robot stops in the middle of the straight section
     # set the last current_difference equal to the current current_difference for derivative steering
 
     last_difference = current_difference
 
     # if the steering variable is higher than the max turn degree for the servo, set it to the max turn degree
     # move the motors using the variables
+
     pw = pwm(servo_angle)
     Board.setPWMServoPulse(6, DC_SPEED, 100)
     Board.setPWMServoPulse(1, pw, 1000)
@@ -388,9 +394,9 @@ while True:
     cv2.imshow("Camera", im)
     if len(sys.argv) > 1 and sys.argv[1] == "Debug":
         cv2.imshow("Camera", im)
-        
     # if the number of actions to the straight section has been met, stop the car
-    if cv2.waitKey(1) == ord("q") or action_counter >= ACTIONS_TO_STRAIGHT: 
+
+    if cv2.waitKey(1) == ord("q") or action_counter >= ACTIONS_TO_STRAIGHT:
         stop()
 
         break
